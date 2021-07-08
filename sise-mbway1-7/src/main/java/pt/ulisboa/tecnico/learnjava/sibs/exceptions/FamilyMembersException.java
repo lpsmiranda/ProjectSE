703268ec11ee23e1fb8f7fe8330df9c2bd2d71e7
lpsmiranda <87022522+lpsmiranda@users.noreply.github.com>
2021-07-08 15:17:50 +0100

    Delete sise-mbway1-7 directory

--- sise-mbway1-7/src/main/java/pt/ulisboa/tecnico/learnjava/sibs/exceptions/FamilyMembersException.java
+++
@@ -1,8 +0,0 @@
-package pt.ulisboa.tecnico.learnjava.sibs.exceptions;
-
-public class FamilyMembersException extends Exception{
-	public FamilyMembersException(String message) {
-		super(message);
-	}
-
-}
